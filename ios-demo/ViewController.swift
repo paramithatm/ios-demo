16655afa75176199170815b77ba0eb1bba26c9b5
Paramitha <paramithatm@gmail.com>
2018-11-02 22:01:35 +0700

    Initial Commit

---
+++ ios-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-demo
+//
+//  Created by Paramitha on 02/11/18.
+//  Copyright © 2018 mitha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
